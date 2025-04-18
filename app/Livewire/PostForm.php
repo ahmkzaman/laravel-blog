950691fcc482f86421acb916cf62d472f3bc2ef8
ahmkzaman <kzaman.ahm@gmail.com>
2025-04-18 14:40:00 +0600

    Route for post added

--- app/Livewire/PostForm.php
+++ app/Livewire/PostForm.php
@@ -2,12 +2,63 @@
 
 namespace App\Livewire;
 
+use App\Models\Tag;
+use Illuminate\Support\Str;
+use App\Models\Post;
 use Livewire\Component;
 
 class PostForm extends Component
 {
+    public $post;
+    public $title;
+    public $content;
+    public $tags;
+    public $selectedTags = [];
+
+    protected $rules = [
+        'title' => 'required|string|min:5',
+        'content' => 'required|string|min:10',
+        'selectedTags' => 'array',
+    ];
+
+    public function mount($post = null)
+    {
+        $this->post = $post;
+
+        if ($post) {
+            $this->post = $post;
+            $this->title = $post->title;
+            $this->content = $post->content;
+            $this->selectedTags = $post->tags()->pluck('id')->toArray();
+        }
+    }
+
+    public function save()
+    {
+
+        $this->validate();
+        $isNew = !$this->post;
+
+        if ($isNew) {
+            $this->post = new Post();
+            $this->post->user_id = auth()->id();
+        }
+
+        $this->post->title = $this->title;
+        $this->post->slug = Str::slug($this->title);
+        $this->post->content = $this->content;
+        $this->post->is_published = true;
+        $this->post->published_at = now();
+        $this->post->save();
+        $this->post->tags()->sync($this->selectedTags);
+        session()->flash('message', $isNew ? 'Post created successfully.' : 'Post updated successfully.');
+        return redirect()->route('posts.show', $this->post);
+    }
     public function render()
     {
-        return view('livewire.post-form');
+        $allTags = Tag::all();
+        return view('livewire.post-form', [
+            'allTags' => $allTags,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Livewire\Settings\Appearance;
-use App\Livewire\Settings\Password;
+use App\Models\Post;
 use App\Livewire\Settings\Profile;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Appearance;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,4 +23,16 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth'])->group(function () {
+    Route::get('/posts/create', [PostController::class, 'create'])
+        ->name('posts.create');
+    Route::get('/post/{post}/edit  ', [PostController::class, 'edit'])
+        ->name('posts.edit');
+});
+
+Route::get('/posts', [PostController::class, 'index'])
+    ->name('posts.index');
+Route::get('/posts/{post}', [PostController::class, 'show'])
+    ->name('posts.show');
+
+require __DIR__ . '/auth.php';
